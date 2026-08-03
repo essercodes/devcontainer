af8d22f829c46480b484ddfdca42ca0aa8961cfc
Max Esser <max@esser.codes signingkey = ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCdWqfMb1oJQVsMl4Ve73vZpoFpmpQnAYX07z/yKHAJKE0Lnc1IvGDGDysw5PW0UpMbNNxQlTCjnEl85JSzjKBQ6cBvQ9XybNEniRUPGniV/j8lLe1AjwN/9hc84zI+cJwt7yHpayUNgrLXz+heWVTXHuVSy7jXpLvFg1NH0hTMJRANL3KUuvIrI9GvYxDmxeuxqFpceDeMyYJ9YSAVIzLtddjtzN/Y0aBvOgD6Jw6QLZ2c96Ly11j6LNuPhZyqWAf8i5KurCCf0AQnDKl9vGHDhkuwlMBMa/FSB2N2fb18KXCHpHtRypJDNuxeTVUVrmOhWAOCiCZKodaPvtF7SC8CeqpvLOqHpKRLCVZtWZYdg9gesCOP/5BUCASZhlrqeA1pmc0SFB753HgxJrHWG1f/UR+KfMP4ls70ZrHpevUpeGvVdS4yZQ7CDEZWoVnn74QyLJKGF959KWRn1UPbWKf+1P5+5fxcxKX6gmMrNeXH7DK/mWcOvhjz8K2Aumlk9bM=>
2026-08-02 21:57:33 -0500

    Added tmux key binds to move between panes with <M-h><M-j><M-k><M-l>

--- features/src/tmux/install.sh
+++ features/src/tmux/install.sh
@@ -96,9 +96,15 @@ if ! grep -qF "$CONF_MARKER" "$TMUX_CONF" 2>/dev/null; then
     cat << EOF >> "$TMUX_CONF"
 $CONF_MARKER
 set -g mouse on
+
 set-window-option -g mode-keys vi
 bind-key -T copy-mode-vi v send -X begin-selection
 bind-key -T copy-mode-vi V send -X select-line
+
+bind-key -n M-h select-pane -L
+bind-key -n M-j select-pane -D
+bind-key -n M-k select-pane -U
+bind-key -n M-l select-pane -R
 EOF
     echo "Status bar disabled in ${TMUX_CONF}"
 fi

36f870755cb885402471a4c67426af1737a9c87d
Max Esser <max@esser.codes signingkey = ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCdWqfMb1oJQVsMl4Ve73vZpoFpmpQnAYX07z/yKHAJKE0Lnc1IvGDGDysw5PW0UpMbNNxQlTCjnEl85JSzjKBQ6cBvQ9XybNEniRUPGniV/j8lLe1AjwN/9hc84zI+cJwt7yHpayUNgrLXz+heWVTXHuVSy7jXpLvFg1NH0hTMJRANL3KUuvIrI9GvYxDmxeuxqFpceDeMyYJ9YSAVIzLtddjtzN/Y0aBvOgD6Jw6QLZ2c96Ly11j6LNuPhZyqWAf8i5KurCCf0AQnDKl9vGHDhkuwlMBMa/FSB2N2fb18KXCHpHtRypJDNuxeTVUVrmOhWAOCiCZKodaPvtF7SC8CeqpvLOqHpKRLCVZtWZYdg9gesCOP/5BUCASZhlrqeA1pmc0SFB753HgxJrHWG1f/UR+KfMP4ls70ZrHpevUpeGvVdS4yZQ7CDEZWoVnn74QyLJKGF959KWRn1UPbWKf+1P5+5fxcxKX6gmMrNeXH7DK/mWcOvhjz8K2Aumlk9bM=>
2026-08-03 19:53:59 -0500

    getting opencode server password from op

--- templates/src/neovim-basic/.devcontainer/bin/set_shortcuts.sh
+++ templates/src/neovim-basic/.devcontainer/bin/set_shortcuts.sh
@@ -6,6 +6,7 @@ EOF
 OPENCODE_ENV_FILE="${CONTAINER_WORKSPACE_FOLDER}/.devcontainer/opencode.env"
 cat <<EOF > "$OPENCODE_ENV_FILE"
 OPENROUTER_API_KEY = op://Devcontainer/OPENROUTER_API_KEY/credential
+OPENCODE_SERVER_PASSWORD = op://Devcontainer/OPENCODE_SERVER_PASSWORD/credential
 EOF
 
 shortcut_na() { eval "$(command shortcut_as_non_admin.sh "$@")"; }
@@ -25,4 +26,3 @@ shortcut_na -c cargo
 opencode_serve() {
     opencode serve --hostname localhost --port 4096
 }
-
